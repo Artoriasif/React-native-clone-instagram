2ba43f667ddf0ada7ace29e97626cbe6464348dd
Pedro <pedrohh0012@icloud.com>
2022-04-20 10:58:17 -0300

    Clone instagram

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+import Routes from './src/Routes';
+// import { NavigationContainer} from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Ionicons from 'react-native-vector-icons/Ionicons'
+// import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Feed from "./src/screens/Feed";
+
+
+// const Tab = createBottomTabNavigator();
+export default function App(){
+  return(
+        <Routes/>
+        //obrigado jao filipi
+  );
+}
